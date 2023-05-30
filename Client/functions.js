db7fdc869a27ae3a4f07a889cfa7d7387f076130
CarsonAnderson1 <andersonc20@spu.edu>
2023-05-29 18:15:29 -0700

    get, push, and delete working

--- Client/functions.js
+++ Client/functions.js
@@ -1,86 +1,115 @@
 // The HTTP Library
 // Will hold api fetch commands
-class httpLibrary{
-    
+class httpLibrary {
     // FINISHED
-    async get(){
-        try{
-            let response = await fetch("/tm/v0/tasks");
-            console.log(response);
-            let tasks = await response.json();
-            console.log(tasks);
-            if(response.ok){
-                console.log("returned tasks");
-                return tasks;
-            }
-            else{
-                console.log("did not return tasks");
-                document.getElementById("booksDisplay").innerHTML = "Error In Getting Data";
-            }
+    async get() {
+      try {
+        let response = await fetch("/tm/v0/tasks");
+        console.log(response);
+        let tasks = await response.json();
+        console.log(tasks);
+        if (response.ok) {
+          console.log("returned tasks");
+          return tasks;
+        } else {
+          console.log("did not return tasks");
+          document.getElementById("booksDisplay").innerHTML = "Error In Getting Data";
         }
-        catch(error){
-            console.log(error.toString());
-        }
-        
+      } catch (error) {
+        console.log(error.toString());
+      }
     }
-    // TODO
-    async post(data){
+    // FINISHED
+    async post(data) {
+      try {
+        const taskData = { name: data }
+        const dat = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(taskData), // Stringify data before sending
+        };
+  
+        console.log("Post Data: ", data);
+        let response = await fetch("/tm/v0/tasks", dat);
+        let postData = await response.json();
+        console.log("Response: ", response);
+        console.log("Post Data: ", postData);
+        return postData;
+      } catch (exception) {
+        console.log("Error: ", exception);
+      }
+    }
+    async delete(id) {
         try {
-            const dat = {
-                method: "POST",
-                headers: { "content-type": "application/json"},
-                body: JSON.stringify(data)
-            }
-            console.log("data body" + dat.body);
-            let response = await fetch("/tm/v0/tasks", dat);
-            let postData = await response.json();
-            console.log("Post Data " + dat);
-            return postData;
+            const taskData = { _id: id }
+            const deleteMethod = {
+                method: 'DELETE',
+                headers: {"content-type": "application/json"},
+                body: JSON.stringify(taskData)
+                }
+                let response = await fetch(`/tm/v0/tasks/${id}`, deleteMethod);
+                return response;
         }
-        catch(exception) {
-            console.log(exception.toString());
+        catch (error) {
+            console.log("Did not delete properly on front end, ", error)
         }
     }
-}
-
-const newLibrary = new httpLibrary();
-// Listener for the HTML buttons
-window.addEventListener('DOMContentLoaded', async () => {
-      /* Post Handler TODO*/
-      document.getElementById('add').addEventListener('click', async (event)=> {
+  }
+  
+  const newLibrary = new httpLibrary();
+  // Listener for the HTML buttons
+  window.addEventListener("DOMContentLoaded", async () => {
+    /* Post Handler*/
+    document.getElementById("add").addEventListener("click", async (event) => {
+      const item = document.getElementById("listitem").value; // sets item to the inserted value
+      if (item == "") {
         event.preventDefault();
-        const item = document.getElementById('listitem').value; // sets item to the inserted value
-        if(item == ""){
-            event.preventDefault();
-            document.getElementById("response").style.color = "red";
-            document.getElementById("response").innerHTML = `<p>Added task is empty </p>`
-            return;
-        }
-        try {
-            console.log("Item posted");
-            await newLibrary.post(item);  // posts item to list.json 
-        }
-        catch (err) {
-            console.log("Post did not work" + err);
-        }
-      })
-
-      /* Delete Handler TODO*/ 
-      document.getElementById('delete').addEventListener('click', async (event)=> {
-        
-      })
-});
-
-async function GetList(){ // This will get the data from list.json 
-    const tasks = await newLibrary.get(); // Fetches from list
-    let output = "<ol>";
-    if (tasks.task.length === 0)
+        document.getElementById("response").style.color = "red";
+        document.getElementById("response").innerHTML = `<p>Added task is empty </p>`;
         return;
-    for (const item of tasks.task){
-        output += `<li>${item.name}</li>`;
+      }
+      try {
+        console.log("Item posted: ", item);
+        await newLibrary.post(item); // posts item to list.json
+      } catch (err) {
+        console.log("Post did not work: ", err);
+      }
+    });
+  
+    /* Delete Handler*/
+    document.getElementById("list").addEventListener("click", async (event) => {
+        const button = event.target;
+        if (button.classList.contains("delete-button")) {
+          console.log("Deleted element");
+          const toDelete = button.getAttribute("data-id");
+          console.log(toDelete);
+          try {
+            await newLibrary.delete(toDelete);
+          } catch (err) {
+            console.log("Delete did not work: ", err);
+          }
+        }
+      });
+  });
+  
+  /* Gets List From DB */
+  async function GetList() {
+    // This will get the data from list.json
+    const tasks = await newLibrary.get(); // Fetches from list
+    let output = "<ul>";
+    if (tasks.task.length === 0) return;
+    for (const item of tasks.task) {
+      output += 
+        `<li>
+            <div>
+                <p>${item.name}</p> 
+                <button type = "submit" class="delete-button" data-id = ${item._id}> Delete </button>
+            </div>
+        </li>`;
     }
-    output += "</ol>"
+    output += "</ul>";
     document.getElementById("list").innerHTML = output;
-}
-GetList();
-
+  }
+  GetList();
--- app.js
+++ app.js
@@ -4,42 +4,67 @@ const connectDB = require("./connect");
 const port = 5001;
 const appName = "Task Manager";
 
-
-// MiddleWare
+// Middleware
 app.use(express.static("./Client"));
 app.use(express.json()); // Converts all info to JSON
 const tasks = require("./task"); // Imports Model
-app.get("/tm/v0/tasks", async (req,res) => {
-    try {
-        const task = await tasks.find();
-        res.status(200).json({task});
-    }
-    catch (error) {
-        res.status(500).json({msg: error});
-    }
-})
+
 app.post("/tm/v0/tasks", async (req, res) => {
-    console.log("got here");
+  try {
+    const task = new tasks(req.body);
+    await task.save();
+    console.log("Task saved: ", task);
+    res.status(201).json({ task });
+  } catch (error) {
+    console.log("Error saving task: ", error);
+    res.status(500).json({ msg: error });
+  }
+});
+
+app.get("/tm/v0/tasks", async (req, res) => {
+  try {
+    const task = await tasks.find();
+    console.log("Retrieved tasks: ", task);
+    res.status(200).json({ task });
+  } catch (error) {
+    console.log("Error retrieving tasks: ", error);
+    res.status(500).json({ msg: error });
+  }
+});
+
+app.get("/tm/v0/tasks/:id", async (req, res) => {
     try {
-        const task = await tasks.create(req.body);
-        await task.save();
-        res.status(200).json(task);
-      } catch (error) {
-        res.status(500).json({ error: "Failed to create task" });
-      }
-})
+      const { id } = req.params;
+      const task = await tasks.findById(id);
+      console.log("Retrieved task: ", task);
+      res.status(200).json({ task });
+    } catch (error) {
+      console.log("Error retrieving task: ", error);
+      res.status(500).json({ msg: error });
+    }
+  });
+  
+app.delete("/tm/v0/tasks/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      await tasks.findByIdAndDelete(id);
+      res.status(200).json({ msg: "Task deleted successfully" });
+    } catch (error) {
+      console.log("Error deleting task: ", error);
+      res.status(500).json({ msg: error });
+    }
+  });
 
 // Start the Server
 const start = async () => {
-    try{
-        await connectDB();
-        app.listen(port, () => {
-            console.log(`${appName} is listening on port ${port}`)
-        })
-    }
-    catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    await connectDB();
+    app.listen(port, () => {
+      console.log(`${appName} is listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 start(); // Calls Async Start
